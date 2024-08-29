3127d83d77f5d151d8cf7025cdd14baaa4c16757
saifgame3 <saifgamevpv@gmail.com>
2024-08-29 18:34:03 +0200

    final project

--- tokyo.js
+++ tokyo.js
@@ -1,2 +1,8 @@
 
 
+function changeContent(title , image ,description) {
+    document.getElementById('title').innerHTML = title;
+    document.getElementById('image').src = image;
+    document.getElementById('description').innerHTML = description
+
+}
